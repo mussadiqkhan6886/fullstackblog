d9a984167abe8974b68747dcd715be2bd812097b
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-08 04:00:56 +0500

    started backend connecting to mongodb

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header'
 import React from 'react'
 
+// axios install fix data download assets react-toastify 
+// mongoose
+
 const Home = () => {
   return (
     <>
