1a0edb1e99fa0597f3490b23fcb2268f4d049a27
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-08 00:45:31 +0500

    started header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <main>
+      <Header />
+    </main>
   )
 }
 
